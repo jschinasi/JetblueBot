5e70fff2ea48d22feec4fbe84c0b69551e6a6ec4
Jake Schinasi <Jake.Schinasi@ibm.com>
2024-05-21 20:22:59 -0400

    change

--- Jetblue_International.py
+++ Jetblue_International.py
@@ -23,7 +23,7 @@ def check_in(last_name, confirmation_code):
 
     try:
         # Find and fill in the last name field
-        last_name_field = WebDriverWait(driver, 5).until(
+        last_name_field = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "/html/body/jb-app/main/jb-search/jb-search-form/div[2]/div/form/div/div/jb-form-field-container[1]/div/div/input"))
         )
         last_name_field.clear()  # Clear existing text (if any)
@@ -99,7 +99,7 @@ def check_in(last_name, confirmation_code):
             EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-traveler-information/jb-additional-information/form/div[2]/jb-transition-button/button"))
         )
         continue_button5.click()
-        print("Clicked on fourth Continue button")
+        print("Clicked on 5th Continue button")
 
         # 2nd additional traveller info
         # Wait for the 6th "Continue" button to be clickable (additional traveller)
@@ -107,7 +107,7 @@ def check_in(last_name, confirmation_code):
             EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-traveler-information/jb-passport-information/form/div[2]/jb-transition-button/button"))
         )
         continue_button6.click()
-        print("Clicked on fourth Continue button")
+        print("Clicked on 6th Continue button")
 
         # Interact with the country dropdown
         dropdown = WebDriverWait(driver, 10).until(
@@ -126,19 +126,19 @@ def check_in(last_name, confirmation_code):
         actions.move_to_element(country_option).click().perform()
         print("Selected United States option using ActionChains")
 
-        # Wait for the 5th "Continue" button to be clickable (additional traveller)
+        # Wait for the 7th "Continue" button to be clickable (additional traveller)
         continue_button7 = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-traveler-information/jb-additional-information/form/div[2]/jb-transition-button/button"))
         )
         continue_button7.click()
-        print("Clicked on fourth Continue button")
+        print("Clicked on 7th Continue button")
 
         # bags continue
         continue_button8 = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-baggage-info/div/jb-loading-container/div/jb-transition-button/button"))
         )
         continue_button8.click()
-        print("Clicked on 7th Continue button")
+        print("Clicked on 8th Continue button")
     except Exception as e:
         print(f"Check-in failed: {str(e)}")
     
@@ -157,7 +157,7 @@ def job():
     check_in(last_name, confirmation_code)
 
 # Schedule the job to run at 11:59:05 PM on Monday
-schedule.every().monday.at("20:40:59").do(job)
+schedule.every().tuesday.at("17:40:00").do(job)
 
 # Run the scheduler
 while True:
