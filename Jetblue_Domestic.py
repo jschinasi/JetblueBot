5b8b5edd2d1273a6bc7034def3c39cb758eb6975
Jake Schinasi <Jake.Schinasi@ibm.com>
2024-10-03 23:07:48 -0400

    Revisions to domestic checkin

--- Jetblue_Domestic.py
+++ Jetblue_Domestic.py
@@ -41,14 +41,14 @@ def check_in(last_name, confirmation_code):
 
         print("Check-in successful!")
 
-        # select all travellers
-        select_all_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-passengers/div[2]/div/div/button"))
-        )
+        # # select all travellers
+        # select_all_button = WebDriverWait(driver, 10).until(
+        #     EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-passengers/div[2]/div/div/button"))
+        # )
 
-        # Click select all travellers
-        select_all_button.click()
-        print("Clicked on select all travellers")
+        # # Click select all travellers
+        # select_all_button.click()
+        # print("Clicked on select all travellers")
 
         # Wait for the first "Continue" button to be clickable
         continue_button1 = WebDriverWait(driver, 10).until(
@@ -70,17 +70,15 @@ def check_in(last_name, confirmation_code):
 
         print("Clicked on second Continue button")
 
-        # Wait for 3rd "Continue" button to be clickable test for hazard
+        # Wait for 3rd "Continue" button to be clickable test for bags
         continue_button3 = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-hazmat/jb-hazmat-form/jb-transition-button/button"))
+            EC.element_to_be_clickable((By.XPATH, "/html/body/jb-app/main/jb-baggage-info/div/jb-loading-container/div/jb-transition-button/button"))
         )
         # Click the 3rd "Continue" button
         continue_button3.click()
 
     except Exception as e:
         print(f"Check-in failed: {str(e)}")
-
-
 
     # Keep the browser window open for inspection
     input("Press Enter to close the browser...")
@@ -91,13 +89,12 @@ def check_in(last_name, confirmation_code):
 def job():
     # Enter your last name and confirmation code here
     last_name = "Schinasi"
-    confirmation_code = "FGOSNS"
+    confirmation_code = "DIHNEW"
 
     check_in(last_name, confirmation_code)
 
 # Schedule the job to run at 11:59:05 PM on Saturday, March 30
-schedule.every().monday.at("16:02:10").do(job)
-2
+schedule.every().thursday.at("23:05:00").do(job)
 # Run the scheduler
 while True:
     schedule.run_pending()
